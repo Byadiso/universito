21fb5e3432724b275b3e1c107c15895782ae4063
Byadiso <byadiso@gmail.com>
2022-01-17 01:51:48 +0100

    debugging syle and adding online check

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -1,22 +1,24 @@
-const mongoose = require('mongoose');
-const crypto = require ('crypto');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
 
-const {ObjectId } = mongoose.Schema;
+const { ObjectId } = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
-    firstName:{ type:String,trim:true,required:true, maxlength:32  },
-    lastName:{type:String,trim:true,required:true, unique: true },
-    username:{type:String,trim:true,required:true, unique: true },
-    email:{type:String,trim:true,required:true, unique: true },
-    password:{type:String,trim:true,required:true, unique: true },
-    profilePic:{type:String, default: "/images/profilePic.png" },
-    coverPhoto:{ type:String },
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, trim: true, required: true, maxlength: 32 },
+    lastName: { type: String, trim: true, required: true, unique: true },
+    username: { type: String, trim: true, required: true, unique: true },
+    email: { type: String, trim: true, required: true, unique: true },
+    password: { type: String, trim: true, required: true, unique: true },
+    profilePic: { type: String, default: "/images/profilePic.png" },
+    coverPhoto: { type: String },
     likes: [{ type: ObjectId, ref: "Post" }],
     retweets: [{ type: ObjectId, ref: "User" }],
     following: [{ type: ObjectId, ref: "User" }],
-    followers: [{ type: ObjectId, ref: "User" }]    
-    }, 
-    { timestamps: true }
+    followers: [{ type: ObjectId, ref: "User" }],
+    isOnline: { type: Boolean, default: false },
+  },
+  { timestamps: true }
 );
 
 var User = mongoose.model("User", userSchema);
